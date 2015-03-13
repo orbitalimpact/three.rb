b420ef5e461e5b2329bf19c5fbe1c41e37cced1f
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-13 12:24:47 -0400

    added an alias for `render` in WebGLRenderer

--- lib/opal/three/core/webgl_renderer.rb
+++ lib/opal/three/core/webgl_renderer.rb
@@ -6,6 +6,7 @@ module THREE
       @native = `new THREE.WebGLRenderer()`
     end
     
+    alias_native :render
     alias_native :set_size, :setSize
     alias_native :dom_element, :domElement
   end
